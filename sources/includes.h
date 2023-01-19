4c43bed952bd030e99f2e4ae4f030ec06e7652d9
averdon <anthonyverdon10@gmail.com>
2023-01-19 09:44:41 +0100

    compil

--- sources/includes.h
+++ sources/includes.h
@@ -6,7 +6,7 @@
 /*   By: averdon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 23:32:45 by averdon           #+#    #+#             */
-/*   Updated: 2023/01/19 09:22:29 by nlocusso         ###   ########.fr       */
+/*   Updated: 2023/01/19 09:44:07 by averdon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 # include "libft/libft.h"
 # include "minilibx/mlx.h"
-# include "../game/game.h"
 # include "../parsing/parsing.h"
+# include "../game/game.h"
 
 #endif
