4cc6f474f977e942d139c175801fbeb9b9df20e0
averdon <anthonyverdon10@gmail.com>
2023-01-24 13:47:05 +0100

    pikachu works

--- game/parse_image.c
+++ game/parse_image.c
@@ -6,7 +6,7 @@
 /*   By: averdon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 19:22:15 by averdon           #+#    #+#             */
-/*   Updated: 2023/01/24 13:34:22 by averdon          ###   ########.fr       */
+/*   Updated: 2023/01/24 13:46:45 by averdon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,33 +19,24 @@ void	parse_image(t_game *game)
 	int			length;
 	int			y;
 	int			x;
-	int			color;
-	unsigned char		**image;
+	unsigned int color;
+	unsigned int **image;
 	
 	img = ft_calloc(1, sizeof(t_img));
-	img->img = mlx_xpm_file_to_image(game->mlx, "textures/W.xpm", &width, &length);
+	img->img = mlx_xpm_file_to_image(game->mlx, "textures/pikachu.xpm", &width, &length);
 	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
+	image = ft_calloc(256, sizeof(unsigned int *));
 	y = 0;
-	image = ft_calloc(256, sizeof(unsigned char *));
 	while (y < length)
 	{
 		x = 0;
-		image[y] = ft_calloc(256, sizeof(unsigned char));
+		image[y] = ft_calloc(256, sizeof(unsigned int));
 		while (x < width)
 		{
 			color = img->addr[y * img->line_length + x * (img->bits_per_pixel / 8)];
-			if (color == 0)
-				image[y][x] = '0';
-			else
-				image[y][x] = '1';
+			image[y][x] = color;
 			x++;
 		}
-		y++;
-	}
-	y = 0;
-	while (y < length)
-	{
-		printf("%s\n", image[y]);
 		y++;
 	}
 }
