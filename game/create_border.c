cebdee0cb2e7b24e666b8f1fdeef07eebbbd35c5
averdon <anthonyverdon10@gmail.com>
2023-01-26 13:26:40 +0100

    norm

--- game/create_border.c
+++ game/create_border.c
@@ -6,13 +6,13 @@
 /*   By: averdon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 17:58:50 by averdon           #+#    #+#             */
-/*   Updated: 2023/01/26 13:04:15 by averdon          ###   ########.fr       */
+/*   Updated: 2023/01/26 13:12:39 by averdon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int		length_max_in_tab(char **tab)
+int	length_max_in_tab(char **tab)
 {
 	int	i;
 	int	length;
@@ -28,19 +28,12 @@ int		length_max_in_tab(char **tab)
 	return (length);
 }
 
-void	create_border(t_game *game)
+void	add_line(char **new_map, int index, int length)
 {
-	int		i;
 	int		j;
-	int		length;
-	char	**new_map;
 
-	new_map = ft_calloc(3 + ft_len_tab(game->map), sizeof(char *));
-	if (!new_map)
-		return ;
-	length = length_max_in_tab(game->map);
-	new_map[0] = ft_calloc(3 + length, sizeof(char));
-	if (!new_map[0])
+	new_map[index] = ft_calloc(3 + length, sizeof(char));
+	if (!new_map[index])
 	{
 		ft_free_array(new_map);
 		return ;
@@ -48,11 +41,19 @@ void	create_border(t_game *game)
 	j = 0;
 	while (j < length + 2)
 	{
-		new_map[0][j] = '2';
+		new_map[index][j] = '2';
 		j++;
 	}
-	i = 1;
-	while (i < ft_len_tab(game->map) + 1)
+}
+
+void	create_new_tab(t_game *game, char **new_map, int length)
+{
+	int		i;
+	int		j;
+
+	add_line(new_map, 0, length);
+	i = 0;
+	while (++i < ft_len_tab(game->map) + 1)
 	{
 		new_map[i] = ft_calloc(3 + length, sizeof(char));
 		if (!new_map[i])
@@ -61,32 +62,29 @@ void	create_border(t_game *game)
 			return ;
 		}
 		new_map[i][0] = '2';
-		j = 0;
-		while (game->map[i - 1][j])
-		{
+		j = -1;
+		while (game->map[i - 1][++j])
 			new_map[i][j + 1] = game->map[i - 1][j];
-			j++;
-		}
 		while (j < length)
 		{
 			new_map[i][j + 1] = ' ';
 			j++;
 		}
 		new_map[i][j + 1] = '2';
-		i++;
 	}
-	new_map[i] = ft_calloc(3 + length, sizeof(char));
-	if (!new_map[i])
-	{
-		ft_free_array(new_map);
+	add_line(new_map, i, length);
+}
+
+void	create_border(t_game *game)
+{
+	int		length;
+	char	**new_map;
+
+	new_map = ft_calloc(3 + ft_len_tab(game->map), sizeof(char *));
+	if (!new_map)
 		return ;
-	}
-	j = 0;
-	while (j < length + 2)
-	{
-		new_map[i][j] = '2';
-		j++;
-	}
+	length = length_max_in_tab(game->map);
+	create_new_tab(game, new_map, length);
 	free_tab(game->map);
 	game->map = new_map;
 }
--- game/key_functions.c
+++ game/key_functions.c
@@ -6,17 +6,40 @@
 /*   By: averdon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 10:46:41 by averdon           #+#    #+#             */
-/*   Updated: 2023/01/25 19:22:24 by averdon          ###   ########.fr       */
+/*   Updated: 2023/01/26 13:26:18 by averdon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
+int	hit_map_border_forward_backward(t_game *game, int coeff,
+		double function_x(double), double function_y(double))
+{
+	int	x;
+	int	y;
+
+	x = coeff * 10 * (function_x(radian_value(game->player->orientation)));
+	x = (game->player->x + x) / SIZE_BLOCK;
+	y = coeff * 10 * (function_y(radian_value(game->player->orientation)));
+	y = (game->player->y + y) / SIZE_BLOCK;
+	if (game->map[x][y] == '2')
+		return (1);
+	return (0);
+}
+
+int	hit_map_border_left_right(t_game *game, int coeff,
+		double function_x(double), double function_y(double))
+{
+	if (game->map[(int)(game->player->x - coeff * 10 * (function_x(radian_value(game->player->orientation)))) / SIZE_BLOCK][(int)(game->player->y + coeff * 10 * (function_y(radian_value(game->player->orientation)))) / SIZE_BLOCK] == '2')
+		return (1);
+	return (0);
+}
+
 void	move_player(t_game	*game, int keycode)
 {
 	if (keycode == W)
 	{
-		if (game->map[(int)(game->player->x + 10 * (cos(radian_value(game->player->orientation)))) / SIZE_BLOCK][(int)(game->player->y + 10 * (sin(radian_value(game->player->orientation)))) / SIZE_BLOCK] != '2')
+		if (!hit_map_border_forward_backward(game, 1, cos, sin))
 		{
 			game->player->x += 10 * (cos(radian_value(game->player->orientation)));
 			game->player->y += 10 * (sin(radian_value(game->player->orientation)));
@@ -24,7 +47,7 @@ void	move_player(t_game	*game, int keycode)
 	}
 	else if (keycode == S)
 	{
-		if (game->map[(int)(game->player->x - 10 * (cos(radian_value(game->player->orientation)))) / SIZE_BLOCK][(int)(game->player->y - 10 * (sin(radian_value(game->player->orientation)))) / SIZE_BLOCK] != '2')
+		if (!hit_map_border_forward_backward(game, -1, cos, sin))
 		{
 			game->player->x -= 10 * (cos(radian_value(game->player->orientation)));
 			game->player->y -= 10 * (sin(radian_value(game->player->orientation)));
@@ -32,7 +55,7 @@ void	move_player(t_game	*game, int keycode)
 	}
 	else if (keycode == D)
 	{
-		if (game->map[(int)(game->player->x - 10 * (sin(radian_value(game->player->orientation)))) / SIZE_BLOCK][(int)(game->player->y + 10 * (cos(radian_value(game->player->orientation)))) / SIZE_BLOCK] != '2')
+		if (!hit_map_border_left_right(game, 1, sin, cos))
 		{
 			game->player->x -= 10 * (sin(radian_value(game->player->orientation)));
 			game->player->y += 10 * (cos(radian_value(game->player->orientation)));
@@ -40,7 +63,7 @@ void	move_player(t_game	*game, int keycode)
 	}
 	else if (keycode == A)
 	{
-		if (game->map[(int)(game->player->x + 10 * (sin(radian_value(game->player->orientation)))) / SIZE_BLOCK][(int)(game->player->y - 10 * (cos(radian_value(game->player->orientation)))) / SIZE_BLOCK] != '2')
+		if (!hit_map_border_left_right(game, -1, sin, cos))
 		{
 			game->player->x += 10 * (sin(radian_value(game->player->orientation)));
 			game->player->y -= 10 * (cos(radian_value(game->player->orientation)));
